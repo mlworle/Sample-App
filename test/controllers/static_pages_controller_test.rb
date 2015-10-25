87884dca4102ddc6e347c153312fd4cafde5867b
mlworle <mlworle@gmail.com>
2015-10-25 15:20:31 -0400

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,21 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    # 3.4.1-2 renamed ~/rails/blog/app/views/layouts/application.html.erb to ~/rails/blog/layout_file
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    # 3.4.1-2 renamed ~/rails/blog/app/views/layouts/application.html.erb to ~/rails/blog/layout_file
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
+  test "should get about" do
+    get :about
+    assert_response :success
+    # 3.4.1-2 renamed ~/rails/blog/app/views/layouts/application.html.erb to ~/rails/blog/layout_file
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+  end
 end
